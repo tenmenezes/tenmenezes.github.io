7548437cac183f6f60799fe1e973e8e9c57d1dc7
Yago Menezes <tenn.menezes@gmail.com>
2025-10-03 11:15:37 -0300

    alterando conteudo do template

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,11 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://tenmenezes.github.io",
+  build: {
+    assets: "assets",
+    assetsPrefix: "./",
+  },
   vite: {
     plugins: [tailwindcss()],
   },
