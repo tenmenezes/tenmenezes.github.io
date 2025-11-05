e80c1d156c64903e45d567be483901d4bb7675a9
Yago Menezes <tenn.menezes@gmail.com>
2025-11-05 15:15:21 -0300

    iniciando implementação de mudança de tema

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 import icon from "astro-icon";
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
@@ -13,5 +14,5 @@ export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
-  integrations: [icon()],
+  integrations: [icon(), react()],
 });
